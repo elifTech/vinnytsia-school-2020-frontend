2c8fb9378535d428abb6ee74fdb310fe180141fe
undead404 <brute18@gmail.com>
2020-03-09 15:58:08 +0200

    babel regenerator runtime

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   ignore: ['node_modules', 'build'],
   plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        regenerator: true,
+      },
+    ],
     ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-syntax-dynamic-import',
