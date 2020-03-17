eab5b5ae1ccc4446507690067acfe4874aadbbee
Ваше ім'я <maroon4m@gmail.com>
2020-03-17 14:38:49 +0200

    changes after merging with master, fix differends

--- src/routes/chat/Chat.js
+++ src/routes/chat/Chat.js
@@ -1,27 +1,78 @@
-import React, { memo, useState, useCallback } from 'react';
+import React, { memo, useState, useCallback, useEffect } from 'react';
 import get from 'lodash/get';
+import property from 'lodash/property';
 import PropTypes from 'prop-types';
 import useStyles from 'isomorphic-style-loader/useStyles';
-// eslint-disable-next-line css-modules/no-unused-class
+import { useDispatch, useSelector } from 'react-redux';
+
+import Messages from './components/messages';
+import ChatInput from './components/chatInput';
+import {
+  chatAddMessage,
+  chatMessageIsLoading,
+  fetchMessages,
+} from '../../actions/security-chat';
+import socket from '../../utils/socket';
+// import testData from './testData.json';
+
 import s from './Chat.css';
+
+const testUserId = '1';
 
 function Chat() {
   useStyles(s);
-  const [userInput, setUserInput] = useState('');
-  const handleChange = useCallback(event => {
-    console.info(event.target.value);
-    setUserInput(get(event, 'target.value'));
-  }, []);
+  const dispatch = useDispatch();
+  const messages = useSelector(property('chat.messages'));
+  // console.info(messages);
+  useEffect(() => {
+    dispatch(fetchMessages());
+    socket.on('SERVER:NEW_MESSAGE', function reloadMessagesList() {
+      dispatch(fetchMessages());
+    });
+  }, [dispatch]);
+  const initialState = '';
+  const [userInput, setUserInput] = useState(initialState);
+  const handleChange = useCallback(
+    event => {
+      socket.emit('CHAT:TYPING', testUserId);
+      // console.info(event.target.value);
+      setUserInput(get(event, 'target.value'));
+      dispatch(chatMessageIsLoading(true));
+    },
+    [dispatch],
+  );
+
+  const sendUserMessage = useCallback(
+    event => {
+      event.preventDefault();
+      const newMessage = {
+        user: testUserId,
+        text: userInput,
+      };
+      // console.info('Text', newMessage);
+      socket.emit('SERVER:NEW_MESSAGE', newMessage);
+      dispatch(chatAddMessage(newMessage));
+      return setUserInput(initialState);
+    },
+    [dispatch, userInput],
+  );
   return (
-    <div>
+    <div className={s.chatContainer}>
       <div className={s.content}>
         <div className={s.header}>Security Chat</div>
-        <div className={s.messages}>Messages</div>
-        <input
-          className={s.messages}
-          onChange={handleChange}
-          type="text"
-          value={userInput}
+        <div className={s.messagesContent}>
+          <div className={s.messages}>
+            {!messages || null || undefined ? (
+              <span>LOADING</span>
+            ) : (
+              <Messages items={messages} />
+            )}
+          </div>
+        </div>
+        <ChatInput
+          handleChange={handleChange}
+          sendUserMessage={sendUserMessage}
+          userInput={userInput}
         />
       </div>
     </div>
@@ -29,10 +80,10 @@ function Chat() {
 }
 Chat.whyDidYouRender = true;
 Chat.prototype = {
-  name: PropTypes.string,
+  Messages: PropTypes.object,
 };
 Chat.defaultProps = {
-  name: '',
+  Messages: {},
 };
 
 export default memo(Chat);
