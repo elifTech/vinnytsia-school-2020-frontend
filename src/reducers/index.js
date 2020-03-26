d0fddd65de0cf56a3682302e32f7139d9484b3c2
Ваше ім'я <maroon4m@gmail.com>
2020-03-26 15:07:26 +0200

    redux-form example merged

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,6 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import as from './as';
-import calcTable from './calc-table';
-import chat from './chat';
 
 export default combineReducers({
-  as,
-  calcTable,
-  chat,
   form: formReducer,
 });
