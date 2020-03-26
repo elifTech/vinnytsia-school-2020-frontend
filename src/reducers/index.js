34bbb1dbc472cbcb08c9ddd893b81c7b1c4478c6
Ваше ім'я <maroon4m@gmail.com>
2020-03-26 15:28:38 +0200

    merge branch master 24, 2020

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import chat from './chat';
 
 export default combineReducers({
+  chat,
   form: formReducer,
 });
